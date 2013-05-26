a0b20ddf4b43c9125a81f75203e136350e3ab8a7
Teodor Mihai Moldovan <moldovan@cs.berkeley.edu>
2013-05-25 23:06:04 -0700

    cartpole batch test. .1 1.0

--- cartpole.py
+++ cartpole.py
@@ -142,7 +142,7 @@ class Tests(unittest.TestCase):
         a = CartPole()
 
         hvdp = learning.OnlineVDP(Distr(), 
-                w=.01, k = 80, tol=1e-4, max_items = 10000)
+                w=.1, k = 80, tol=1e-4, max_items = 10000)
 
         planner = Planner(.02,.5,h_cost=1.0)
         
