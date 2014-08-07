a2609c866a400d99053aa6a991509eaf68f6f7ea
Zoe McCarthy <zoemccarthy12@gmail.com>
2014-08-07 00:33:09 -0500

    tests for robotarm work now

--- makefile
+++ makefile
@@ -4,7 +4,8 @@ all: ft
 #CMD = python test.py TestsAutorotation.test_learning
 #CMD = python test.py TestsAutorotation.test_pp_iter
 #CMD = python test.py TestsHeli.test_accs
-CMD = python cartpole.py TestsCartpole.test_accs
+CMD = python robotarm.py TestsRobotArm.test_learning
+#CMD = python robotarm.py TestsRobotArm.test_accs
 plots:
 	python plots.py
 noft: 
--- robotarm.py
+++ robotarm.py
@@ -3,8 +3,162 @@ from sympy import sin, cos, diag,log,exp, sqrt, nsimplify, trigsimp
 from sympy import Matrix as mat
 from sympy.physics.mechanics import *
 from math import pi
-from pydy.codegen.code import generate_ode_function
-#from IPython import embed
+from numpy import zeros, array, linspace, ones, deg2rad
+from IPython import embed
+
+from planning import *
+from test import TestsDynamicalSystem, unittest
+
+
+class RobotArmBase:
+    #collocation_points = 15
+    tol = 0.000001
+    def initial_state(self):
+        state = np.zeros(self.nx)
+        state[1] = deg2rad(-89)
+        #state += .25*np.random.normal(size = self.nx)
+        return state 
+        
+    def symbolics(self, forward_gen=False):
+
+
+        state  = (t0,  t1,  t2, w0, w1, w2)  = dynamicsymbols("""
+                        t0, t1, t2, w0, w1, w2
+                    """)
+        
+        dstate = (t0d, t1d, t2d, w0d, w1d, w2d) = dynamicsymbols("""
+                        t0, t1, t2, w0, w1, w2
+                    """, 1)
+
+        controls = (u0, u1, u2) = dynamicsymbols('u0, u1, u2')
+
+        symbols = dstate + state + controls
+
+        g = 9.81
+        m1, m2 = 5.0, 3.0
+        r1, r2 = 1., 0.75
+        h1, h2 = 5.0, 2.25
+        f0, f1, f2 = 5.0, 3.5, 2.7
+
+
+        def dyn_full():
+
+            # frame convention:
+            # world frame is fixed
+            # frame0 is pre-joint0
+            # frame1 is pre-joint1
+            # frame1inter is pre-joint1 after the theta rotation
+            # frame2 is pre-joint2
+            # frame3 is post-joint2
+
+            W = ReferenceFrame('WorldFrame') # worldframe is at base of robot but not moving
+
+            frame0 = W.orientnew('frame0',           'Axis', [0.0, W.z])
+            frame0.set_ang_vel(W, 0.0 * frame0.x)
+            frame1inter = frame0.orientnew('frame1inter', 'Axis', [t0, frame0.z])
+            frame1inter.set_ang_vel(frame0, t0d * frame0.z)
+            frame1 = frame1inter.orientnew('frame1',      'Axis', [-pi / 2, frame1inter.x])
+            frame1.set_ang_vel(frame1inter, 0.0 * frame1inter.z)
+            frame2 = frame1.orientnew('frame2',           'Axis', [t1, frame1.z])
+            frame2.set_ang_vel(frame1, t1d * frame1.z)
+            frame3 = frame2.orientnew('frame3',           'Axis', [t2, frame2.z])
+            frame3.set_ang_vel(frame2, t2d * frame2.z)
+
+            # point convention: 
+            # og is origin in world frame (also base)
+            # point0 is base
+            # point1 is also base (after the first rotational joint)
+            # point2 is at the intersection of the first and second links
+            # point3 is at the end of the second link
+
+            og = Point('origin') # origin, center of robot base and site of joint0 and joint1
+
+            point0 = og.locatenew('point0', 0.0 * W.z)
+            point0.set_vel(frame0, 0.0 * frame0.x)
+            point1 = point0.locatenew('point1', 0.0 * frame0.z)
+            point1.set_vel(W, 0.0 * W.x)
+            point2 = point1.locatenew('point2', h1 * frame2.x)
+            point2.v2pt_theory(point1, W, frame2)
+            point3 = point2.locatenew('point3', h2 * frame3.x)
+            point3.v2pt_theory(point2, W, frame3)
+
+            # inertial frames and centers of mass for links 1 and 2
+            link1_center_frame, link1_center, link1_inertia, link1_body = \
+                cylinder(m1, r1, h1, index=1)
+            link2_center_frame, link2_center, link2_inertia, link2_body = \
+                cylinder(m2, r2, h2, index=2)
+
+            link1_center_frame.orient(frame2, 'Axis', [0.0, frame2.z])
+            link1_center_frame.set_ang_vel(frame1, t1d * frame1.z)
+            link2_center_frame.orient(frame3, 'Axis', [0.0, frame3.z])
+            link2_center_frame.set_ang_vel(frame2, t2d * frame2.z)
+
+            link1_center.set_pos(point1, h1 / 2.0 * frame2.x)
+            link1_center.v2pt_theory(point1, W, frame2)
+            link2_center.set_pos(point2, h2 / 2.0 * frame3.x)
+            link2_center.v2pt_theory(point2, W, frame3)
+
+            kr = [w0 - t0d, w1 - t1d, w2 - t2d]
+            
+            BodyList  = [link1_body, link2_body]
+            ForceList = [(link1_center, -g * m1 * W.z), # gravity 1
+                         (link2_center, -g * m2 * W.z), # gravity 2
+                         (link1_center_frame, f0 * u0 * W.z + f1 * u1 * frame1.z - f2 * u2 * frame2.z), # link1 FBD
+                         (link2_center_frame, f2 * u2 * frame2.z) # link2 FBD
+                         ] 
+
+            coords = [t0, t1, t2]
+            speeds = [w0, w1, w2]
+
+
+            KM = KanesMethod(W, coords, speeds, kd_eqs=kr)
+
+            (fr, frstar) = KM.kanes_equations(ForceList, BodyList)
+
+            if forward_gen:
+                mm = KM.mass_matrix_full
+                msh0, msh1 = mm.shape
+                tol = self.tol
+                mass = mat([[trigsimp(nsimplify(mm[i, j],      tolerance=tol)) for j in range(msh1)] for i in range(msh0)])
+
+                forc= KM.forcing_full
+                fsh0, fsh1 = forc.shape
+                forcing = mat([[trigsimp(nsimplify(forc[i, j], tolerance=tol)) for j in range(fsh1)] for i in range(fsh0)])
+
+                from pydy.codegen.code import generate_ode_function
+                right_hand_side = generate_ode_function(mass, forcing, [], coords, speeds, controls)
+
+
+
+            return locals()
+
+        def dyn():
+
+            locals_dict = dyn_full()
+
+            fr = locals_dict['fr']
+            frstar = locals_dict['frstar']
+            kr = locals_dict['kr']
+
+            frsh0, frsh1 = fr.shape
+            tol = self.tol
+
+            frsimp     = mat([[trigsimp(nsimplify(fr[i, j],     tolerance=tol)) for j in range(frsh1)] for i in range(frsh0)])
+            frstarsimp = mat([[trigsimp(nsimplify(frstar[i, j], tolerance=tol)) for j in range(frsh1)] for i in range(frsh0)])
+
+            dyn = frsimp + frstarsimp
+            # this next line is probably not needed since the frsimp + frstarsimp terms have different variables 
+            # and are thus orthogonal
+            #dynsimp = mat([[trigsimp(nsimplify(dyn[i, j], tolerance=tol)) for j in range(frsh1)] for i in range(frsh0)])
+
+            dyn = mat(list(dyn) + kr)
+
+            return dyn
+
+        def state_target(): 
+            return (t0 - pi / 4, t1 + pi / 4, t2 - pi / 4, w0, w1, w2)
+
+        return locals()
 
 
 def symbolics():
@@ -160,11 +314,22 @@ def cylinder(mass, radius, height, index=None):
     B = RigidBody('Link' + i, cm, F, mass, (I, cm))
     return F, cm, I, B
 
+class RobotArm(RobotArmBase, DynamicalSystem):
+    pass
+
+class TestsRobotArm(TestsDynamicalSystem):
+    DSLearned = RobotArm
+    DSKnown   = RobotArm
 
 if __name__ == '__main__':
     """ to avoid merge conflicts, let's run individual tests 
         from command-line like this:
 	  python cartpole.py Tests.test_accs
     """
-    symbolics()
+    #robot_arm = RobotArmBase()
+    #symbols = robot_arm.symbolics()
+    #dyns = symbols['dyn']
+    #dyns()
+
+    unittest.main()
 
