4da4f18dfc3b8e2ddb1f52ce5bf579949b1dbc44
Zoe McCarthy <zoemccarthy12@gmail.com>
2014-08-06 22:56:08 -0500

    working robot arm visualization/dynamics

--- robotarm.py
+++ robotarm.py
@@ -1,9 +1,10 @@
 import sympy
-from sympy import sin, cos, diag,log,exp, sqrt
+from sympy import sin, cos, diag,log,exp, sqrt, nsimplify, trigsimp
 from sympy import Matrix as mat
 from sympy.physics.mechanics import *
-from IPython import embed
 from math import pi
+from pydy.codegen.code import generate_ode_function
+#from IPython import embed
 
 
 def symbolics():
@@ -25,9 +26,10 @@ def symbolics():
     #uw = (13.20, -9.21, 14.84, -27.5) 
     #fw = (-3.47, -3.06, -2.58, -.048, -.12, -.0005)
     g = 9.81
-    m1, m2 = 1.0, 3.0
-    r1, r2 = 0.5, 0.75
-    h1, h2 = 3.0, 5.25
+    m1, m2 = 5.0, 3.0
+    r1, r2 = 1., 0.75
+    h1, h2 = 5.0, 2.25
+    f0, f1, f2 = 5.0, 1.5, 0.7
 
     # frame convention:
     # world frame is fixed
@@ -43,7 +45,7 @@ def symbolics():
     frame0.set_ang_vel(W, 0.0 * frame0.x)
     frame1inter = frame0.orientnew('frame1inter', 'Axis', [t0, frame0.z])
     frame1inter.set_ang_vel(frame0, t0d * frame0.z)
-    frame1 = frame1inter.orientnew('frame1',           'Axis', [-pi / 2, frame1inter.x])
+    frame1 = frame1inter.orientnew('frame1',      'Axis', [-pi / 2, frame1inter.x])
     frame1.set_ang_vel(frame1inter, 0.0 * frame1inter.z)
     frame2 = frame1.orientnew('frame2',           'Axis', [t1, frame1.z])
     frame2.set_ang_vel(frame1, t1d * frame1.z)
@@ -96,12 +98,41 @@ def symbolics():
 
     
     BodyList  = [link1_body, link2_body]
-    ForceList = [(link1_center, -g * m1 * W.z),
-                 (link2_center, -g * m2 * W.z)]
+    ForceList = [(link1_center, -g * m1 * W.z), # gravity 1
+                 (link2_center, -g * m2 * W.z),
+                 (link1_center_frame, f0 * u0 * W.z + f1 * u1 * frame1.z - f2 * u2 * frame2.z),
+                 (link2_center_frame, f2 * u2 * frame2.z)
+                 #(frame0, f0 * u0 * frame0.z),
+                 #(frame1, f1 * u1 * frame1.z),
+                 #(frame2, f2 * u2 * frame2.z),
+                 ] # gravity 2
 #                 (link1_center_frame, -g * m2 * W.z)] # not done yet
 
+    coords = [t0, t1, t2]
+    speeds = [w0, w1, w2]
 
-    embed()
+
+    #KM = KanesMethod(W, q_ind= coords,
+                        #speeds,
+                        #u_ind=  [u0, u1, u2], 
+                        #kd_eqs=kr )
+    KM = KanesMethod(W, coords, speeds, kd_eqs=kr)
+
+    (fr, frstar) = KM.kanes_equations(ForceList, BodyList)
+    mm = KM.mass_matrix_full
+    msh0, msh1 = mm.shape
+    forc= KM.forcing_full
+    fsh0, fsh1 = forc.shape
+    tol = 0.000001
+
+    #dynsymp = [trigsimp(nsimplify(eq, tolerance=tol)) for eq in fr + frstar]
+    mass = mat([[trigsimp(nsimplify(mm[i, j],      tolerance=tol)) for j in range(msh1)] for i in range(msh0)])
+    forcing = mat([[trigsimp(nsimplify(forc[i, j], tolerance=tol)) for j in range(fsh1)] for i in range(fsh0)])
+
+    right_hand_side = generate_ode_function(mass, forcing, [], coords, speeds, controls)
+
+    return locals()
+
 
 
 def cylinder(mass, radius, height, index=None):
